1523404f517d908c8176558bd060255eec2a46a6
Mellamoberto <app-del-96@hotmail.com>
2023-04-26 13:00:49 +0200

    MediaClase LMarcas 26/04/2023

---
+++ Ejercicio 26 - 27/Saludos.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var color = 'white';
+
+
+function obtieneColor(identificador) {
+    let id = "c" + identificador;
+    let cuadro = document.getElementById(id);    
+    color = getComputedStyle(cuadro).backgroundColor;
+    console.log(color);
+}
+
+function saludar(identificador) {
+    let id = "c" + identificador;
+    let cuadro = document.getElementById(id);
+    cuadro.style.color = color;
+    cuadro.innerHTML = "HOLA";
+}
